372dd0f11558a4466e365b9f0f5092dd129ca379
Kim Seoyul <syq3e7@icloud.com>
2023-09-18 12:04:55 +0900

    자재조달협력회사, 조달계약, 조달계획, 생산계획, 자재조달차트 Entity 수정및 생성

--- src/main/java/com/deligence/deli/domain/CooperatorSupplier.java
+++ src/main/java/com/deligence/deli/domain/CooperatorSupplier.java
@@ -16,25 +16,25 @@ public class CooperatorSupplier {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int supplier_no;    //자재협력회사 일련번호
+    private int supplierNo;    //자재협력회사 일련번호
 
-    private int corporate_registration_no;  //사업자등록번호
+    private int corporateRegistrationNo;  //사업자등록번호
 
-    private String supplier_ceo;    //자재협력회사 대표
+    private String supplierCeo;    //자재협력회사 대표
 
-    private String supplier_email;  //자재협력회사 이메일
+    private String supplierEmail;  //자재협력회사 이메일
 
-    private String supplier_name;   //자재협력화사명
+    private String supplierName;   //자재협력화사명
 
-    private String supplier_phone;  //자재협력회사연락처
+    private String supplierPhone;  //자재협력회사연락처
 
-    private String supplier_address;    //자재협력회사주소
+    private String supplierAddress;    //자재협력회사주소
 
-    private String supplier_status; //계약상태
+    private String supplierStatus; //계약상태
 
 //    @ManyToOne
 //    private DocumentFile documentFile;    //문서파일 일련번호 (document_file_no)
 
-    private String supplier_etc;    //비고
+    private String supplierEtc;    //비고
 
 }
--- src/main/java/com/deligence/deli/domain/MaterialProcurementContract.java
+++ src/main/java/com/deligence/deli/domain/MaterialProcurementContract.java
@@ -18,19 +18,19 @@ public class MaterialProcurementContract extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private int material_procurement_contract_no;
+    private int materialProcurementContractNo;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Materials materials;    //자재일련번호 (material_no)
 
-    private String material_procurement_contract_code;
+    private String materialProcurementContractCode;
 
-    private LocalDate material_procurement_contract_date;
+    private LocalDate materialProcurementContractDate;
 
-    private String material_procurement_contract_state;
+    private String materialProcurementContractState;
 
 //    @ManyToOne(fetch = FetchType.LAZY)
-//    private DocumentFile document_file;   //문서파일일련번호 (document_file_no)
+//    private DocumentFile documentFile;   //문서파일일련번호 (document_file_no)
 
     @ManyToOne(fetch = FetchType.LAZY)
     private CooperatorSupplier cooperatorSupplier;  //자재조달협력회사일련번호 supplier_no
@@ -38,14 +38,14 @@ public class MaterialProcurementContract extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Employee employee;  //사원일련번호 employee_no
 
-    private String material_code;   //자재코드 (검색용)
+    private String materialCode;   //자재코드 (검색용)
 
-    private String material_name;   //자재명 (검색용)
+    private String materialName;   //자재명 (검색용)
 
-    private Long material_supply_price; //공급단가 (검색용)
+    private Long materialSupplyPrice; //공급단가 (검색용)
 
-    private String supplier_name;   //자재협력회사명 (검색용)
+    private String supplierName;   //자재협력회사명 (검색용)
 
-    private String supplier_status; //계약상태 (검색용)
+    private String supplierStatus; //계약상태 (검색용)
 
 }
