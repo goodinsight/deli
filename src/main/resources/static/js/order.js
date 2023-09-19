1920da5736eec71622cf5b6c50595cf676262739
Yang JinYul <devac1519@gmail.com>
2023-09-19 17:01:29 +0900

    발주 등록 페이지 : 계약 정보 가져오는 기능 준비 & 발주코드 자동생성 기능 작업중

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -6,6 +6,25 @@ async function getPlanList({page, size}){
 }
 
 async function getPlan(planNo) {
+
     const response = await axios.get(`/order/register/getPlan/${planNo}`)
+
     return response.data
+}
+
+
+async function getContractList({page, size}){
+
+    //const result = await axios.get(`/order/register/selectContract`, {params: {page, size}})
+
+    //return result.data
+    return null;
+}
+
+async function getContract(contractNo) {
+
+    //const response = await axios.get(`/order/register/getContract/${contractNo}`)
+
+    //return response.data
+    return null;
 }
