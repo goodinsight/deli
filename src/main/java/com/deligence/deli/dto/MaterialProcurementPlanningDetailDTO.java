89342accf3243601723cced1933293227427248c
Kim Seoyul <syq3e7@icloud.com>
2023-09-21 09:02:48 +0900

    조달계획search, searchImpl수정

--- src/main/java/com/deligence/deli/dto/MaterialProcurementPlanningDetailDTO.java
+++ src/main/java/com/deligence/deli/dto/MaterialProcurementPlanningDetailDTO.java
@@ -19,13 +19,9 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class MaterialProcurementPlanningDetailDTO {
 
-    private int materialProcurementPlanNo;   //조달계획일련번호
+    private int materialProcurementPlanNo;   //조달계획 일련번호
 
-    private LocalDateTime modDate;  //수정일
-
-    private LocalDateTime regDate;  //등록일
-
-    @NotNull //@NotEmpty는 String타입에만 붙음. 그 외는 @NotNull
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate procurementDeliveryDate;    //납기일
 
@@ -35,14 +31,14 @@ public class MaterialProcurementPlanningDetailDTO {
     //@NotEmpty
     private String materialProcurementState;  //자재조달상태
 
-    private Materials materials;    //자재일련번호 FK
-//    private List<MaterialsDTO> materials;
+//    private Materials materials;    //자재일련번호 FK
 
     @NotNull
     private int productionPlanNo; //생산계획 일련번호 FK
 
-    private Employee employee;    //사원일련번호 FK
-//    private List<EmployeeJoinDTO> employee;    //사원일련번호 FK
+    private int materialNo; //자재 일련번호
+
+    private String materialType;    //자재타입
 
     @NotEmpty
     private String materialCode;   //자재코드 (검색용)
@@ -50,7 +46,15 @@ public class MaterialProcurementPlanningDetailDTO {
     @NotEmpty
     private String materialName;   //자재이름 (검색용)
 
-    private int materialNo; //자재 일련번호
+//    private Employee employee;    //사원일련번호 FK
+
+    private int employeeNo; //사원 일련번호
+
+    private String employeeName;    //사원 이름 (담당자)
+
+    private LocalDateTime regDate;  //등록일
+
+    private LocalDateTime modDate;  //수정일
 
 
 }
