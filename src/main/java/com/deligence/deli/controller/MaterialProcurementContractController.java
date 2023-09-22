9029899e4a759bef2a788cc8e5b694c34d0d107d
Kim Seoyul <syq3e7@icloud.com>
2023-09-22 12:36:43 +0900

    계약 register controller, html작성, 계획오류수정

--- src/main/java/com/deligence/deli/controller/MaterialProcurementContractController.java
+++ src/main/java/com/deligence/deli/controller/MaterialProcurementContractController.java
@@ -2,6 +2,7 @@ package com.deligence.deli.controller;
 
 import com.deligence.deli.domain.CooperatorSupplier;
 import com.deligence.deli.domain.MaterialProcurementContract;
+import com.deligence.deli.dto.EmployeeSecurityDTO;
 import com.deligence.deli.dto.MaterialProcurementContractDTO;
 import com.deligence.deli.dto.PageRequestDTO;
 import com.deligence.deli.dto.PageResponseDTO;
@@ -10,12 +11,14 @@ import com.deligence.deli.service.MaterialProcurementContractService;
 import com.deligence.deli.service.MaterialsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 //자재조달계약 Controller
 @Controller
@@ -46,6 +49,42 @@ public class MaterialProcurementContractController {
         model.addAttribute("responseDTO", responseDTO);
 
     }
+
+    @GetMapping("/register")    //자재조달계약 employee2
+    public void registerGET(@AuthenticationPrincipal EmployeeSecurityDTO employeeSecurityDTO, Model model) {
+
+        log.info(employeeSecurityDTO);
+
+        model.addAttribute("user", employeeSecurityDTO);
+
+    }
+
+    @PostMapping("register")
+    public String registerPOST(@Valid MaterialProcurementContractDTO materialProcurementContractDTO,
+                               BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        log.info("materialProcurementContact post register........");
+
+        log.info(materialProcurementContractDTO);
+
+        if (bindingResult.hasErrors()) {
+
+            log.info("materialProcurementContract register error");
+
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/materialProcurementContract/register";
+        }
+
+        int materialProcurementContractNo =
+                materialProcurementContractService.register(materialProcurementContractDTO);
+
+        redirectAttributes.addFlashAttribute("result", materialProcurementContractNo);
+
+        return "redirect:/materialProcurementContract/list";
+    }
+
+
 
     //비동기처리 -----------------------------------------------------
     @ResponseBody
--- src/main/resources/static/js/materialProcurementContract.js
+++ src/main/resources/static/js/materialProcurementContract.js
@@ -1,3 +1,31 @@
+async function getMaterialList({page, size}){
+
+    const result = await axios.get(`/materialProcurementContract/register/selectMaterial`, {params: {page, size}})
+
+    return result.data
+}
+
+async function getMaterial(materialsNo) {
+
+    const result = await axios.get(`/materialProcurementContract/register/getMaterial/${materialsNo}`)
+
+    return result.data
+}
+
+async function getSupplierList({page, size}){
+
+    const result = await axios.get(`/materialProcurementContract/register/selectSupplier`, {params: {page, size}})
+
+    return result.data
+}
+
+async function getSupplier(suppliersNo) {
+
+    const result = await axios.get(`/materialProcurementContract/register/getSupplier/${suppliersNo}`)
+
+    return result.data
+}
+
 async function getCodeCount(materialProcurementContractCode){
 
     const result = await axios.get(`/materialProcurementContract/register/getCodeCount/${materialProcurementContractCode}`)
