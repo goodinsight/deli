af2ed3cbb3337aaf6036cef29289c3325ed51980
Yang JinYul <devac1519@gmail.com>
2023-09-22 17:40:59 +0900

    검수 페이지 : 기본적인 수정 기능 구현
    검수 수정에 따른 상태변환 작업예정

--- src/main/resources/static/js/progressInspection.js
+++ src/main/resources/static/js/progressInspection.js
@@ -22,6 +22,7 @@ async function addProgressInspection(piObj) {
 
 async function getProgressInspection(progressInspectionNo) {
     const response = await axios.get(`/progressInspection/${progressInspectionNo}`)
+
     return response.data
 }
 
