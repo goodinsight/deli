ad90b9bdfb950187ba0cbce4474a3d0380980e0d
Kim Seoyul <syq3e7@icloud.com>
2023-09-27 17:51:21 +0900

    제품계약 만들다 말았음

--- src/main/java/com/deligence/deli/domain/ProductContract.java
+++ src/main/java/com/deligence/deli/domain/ProductContract.java
@@ -17,19 +17,20 @@ public class ProductContract extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int productContractNo;  //계약일련번호
+    private int productContractNo;              //계약일련번호
+
+    private String productContractCode;         //제품계약코드
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private Products products;  //제품일련번호 (FK)
+    private Products products;                  //제품일련번호 (FK)
 
-    private int productQuantity;    //제품 수량
+    private String productCode;                 //제품코드 (클라이언트 주문 목록 -> 검색용)
 
-    private LocalDate productDeliveryDate;  //납기일
+    private int productQuantity;                //제품수량 (클라이언트 주문 목록)
 
-    private String productQuotation;    //견적내용
+    private LocalDate productDeliveryDate;      //납기일 (클라이언트 주문 목록)
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private DocumentFile documentFile;    //문서파일일련번호(FK)
+    private String productQuotation;            //견적내용
 
     @ManyToOne(fetch = FetchType.LAZY)
     private CooperatorClient cooperatorClient;  //구매협력회사일련번호 (FK)
@@ -37,9 +38,11 @@ public class ProductContract extends BaseEntity{
     @ManyToOne(fetch = FetchType.LAZY)
     private Employee employee;  //사원일련번호-담당자 (FK)
 
-    private String productCode;     //제품코드
 
     private String clientName;  //구매협력회사명
 
     private String clientStatus;    //계약상태
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private DocumentFile documentFile;    //문서파일일련번호(FK)
 }
