76c966b37541f9029f838e957a92c17b4204f6a7
Yang JinYul <devac1519@gmail.com>
2023-10-06 11:32:12 +0900

    발주 파기, 발주 등록시 코드 필수 입력, 임시적인 제거 기능

--- src/main/java/com/deligence/deli/dto/OrderDTO.java
+++ src/main/java/com/deligence/deli/dto/OrderDTO.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
@@ -15,15 +17,20 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class OrderDTO {
 
+    @NotNull
     private int orderNo;//발주 일련번호
 
+    @NotEmpty
     private String orderCode;//발주 코드
 
+    @NotNull
     private int orderQuantity;//발주 수량
 
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate orderDeliveryDate;//납기일
 
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate orderDate;//발주일
 
@@ -31,8 +38,10 @@ public class OrderDTO {
 
     private String orderEtc; //비고
 
+    @NotNull
     private int materialProcurementPlanNo;//조달계획 일련번호 : FK
 
+    @NotNull
     private int materialProcurementContractNo;//조달계약 일련번호 : FK
 
     private int employeeNo;// 사원 일련번호
